d80bbecf740b8ac02728be747b9e258cd5a1972b
tetsu9923 <tk_kasa@live.jp>
2021-12-27 14:11:51 +0900

    initial commit

--- get-papers.py
+++ get-papers.py
@@ -1,22 +1,24 @@
-import urllib.request
+import argparse
 import datetime
-import requests
 import json
+import os
 import pickle
 import time
+import urllib.request
+
 import feedparser
+import requests
 
 
-def main():
-    # Base api query url
+def main(args):
     base_url = 'http://export.arxiv.org/api/query?'
-    
-    search_query = 'cat:cs.AI+OR+cat:cs.LG+OR+cat:cs.CV+OR+cat:cs.CL+OR+cat:stat.ML'
-    append = True
-    start_idx = 50000
-    max_results = 100
-    n_requests = 10000  # Max number of papers: n_requests*max_results
-    day_minus = -365
+
+    query = args.query
+    start_idx = args.start_idx
+    max_results = args.max_results
+    day_minus = -1*args.day_minus
+    n_requests = args.n_requests
+    append = args.append
 
     target_day = (datetime.date.today() + datetime.timedelta(days=day_minus))
         
@@ -32,12 +34,12 @@ def main():
         abst_list = []
         link_list = []
 
-    print("Current number of papers: {}".format(len(title_list)))
+    print('Initial number of papers: {}'.format(len(title_list)))
 
     n_papers = 0
     break_flag = False
     for i in range(n_requests):
-        query = 'search_query={0}&start={1}&max_results={2}&sortBy=lastUpdatedDate&sortOrder=descending'.format(search_query, start_idx+n_papers, max_results)
+        query = 'search_query={0}&start={1}&max_results={2}&sortBy=lastUpdatedDate&sortOrder=descending'.format(search_query, n_papers+start_idx, max_results)
         # feedparser v4.1
         feedparser._FeedParserMixin.namespaces['http://a9.com/-/spec/opensearch/1.1/'] = 'opensearch'
         feedparser._FeedParserMixin.namespaces['http://arxiv.org/schemas/atom'] = 'arxiv'
@@ -48,19 +50,29 @@ def main():
             
         # parse the response using feedparser
         feed = feedparser.parse(response)
+        total_results = int(feed.feed.opensearch_totalresults)
+
+        if len(feed.entries) != max_results and len(feed.entries)+n_papers+start_idx != total_results:
+            print('Number of responses < max_results')
+            print('Current number of papers: {}'.format(n_papers+start_idx))
+            time.sleep(10)
+            continue
         
         for entry in feed.entries:
             published_date = (entry.updated.split('T'))[0]
-            published_date = datetime.datetime.strptime(published_date, "%Y-%m-%d").date()
+            published_date = datetime.datetime.strptime(published_date, '%Y-%m-%d').date()
             if published_date < target_day:
                 break_flag = True
                 break
             title_list.append(entry.title)
-            abst_list.append(entry.summary.replace("\n", " "))
+            abst_list.append(entry.summary.replace('\n', ' '))
             link_list.append(entry.links[0]['href'])
             n_papers += 1
+            if total_results <= n_papers+start_idx:
+                break_flag = True
+                break
         
-        print("Current number of papers: {}".format(n_papers+start_idx))
+        print('Current number of papers: {}'.format(n_papers+start_idx))
         if break_flag:
             break
         time.sleep(10)
@@ -71,7 +83,7 @@ def main():
         with open('./data/raw_link.pkl', 'wb') as f:
             pickle.dump(link_list, f)
     
-    print("Total number of papers: {}".format(n_papers))
+    print('Total number of papers: {}'.format(len(title_list)))
 
     with open('./data/raw_title.pkl', 'wb') as f:
         pickle.dump(title_list, f)
@@ -82,4 +94,13 @@ def main():
         
         
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--query', type=str, default='cat:cs.LG')
+    parser.add_argument('--start-idx', type=int, default=0)
+    parser.add_argument('--max-results', type=int, default=10000)
+    parser.add_argument('--day-minus', type=int, default=10000)
+    parser.add_argument('--n-requests', type=int, default=10000)
+    parser.add_argument('--append', type=bool, default=False)
+
+    args = parser.parse_args()
+    main(args)
--- similar-search.py
+++ similar-search.py
@@ -1,6 +1,6 @@
-import time
-import torch
 import pickle
+
+import torch
 import numpy as np
 from transformers import AutoTokenizer, AutoModel
 
@@ -9,61 +9,71 @@ def cos_similarity(v1, v2):
     return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
 
 
-top_n = 5
-use_title = True
-use_abst = True
-query_title = "Edge-Level Explanations for Graph Neural Networks by Extending Explainability Methods for Convolutional Neural Networks"
-query_abst = "Graph Neural Networks (GNNs) are deep learning models that take graph data as inputs, and they are applied to various tasks such as traffic prediction and molecular property prediction. However, owing to the complexity of the GNNs, it has been difficult to analyze which parts of inputs affect the GNN model's outputs. In this study, we extend explainability methods for Convolutional Neural Networks (CNNs), such as Local Interpretable Model-Agnostic Explanations (LIME), Gradient-Based Saliency Maps, and Gradient-Weighted Class Activation Mapping (Grad-CAM) to GNNs, and predict which edges in the input graphs are important for GNN decisions. The experimental results indicate that the LIME-based approach is the most efficient explainability method for multiple tasks in the real-world situation, outperforming even the state-of-the-art method in GNN explainability."
+def main(args):
+    top_n = args.top_n
+    query_title = args.title
+    query_abst = args.abst
+    use_title = args.title != ''
+    use_abst = args.abst != ''
+    
+    tokenizer = AutoTokenizer.from_pretrained('allenai/specter')
+    model = AutoModel.from_pretrained('allenai/specter')
+    model = model.to('cpu')
 
-tokenizer = AutoTokenizer.from_pretrained("allenai/specter")
-model = AutoModel.from_pretrained("allenai/specter")
-model = model.to("cpu")
+    with open('./data/raw_title.pkl', 'rb') as f:
+        title_list = pickle.load(f)
+    with open('./data/raw_abst.pkl', 'rb') as f:
+        abst_list = pickle.load(f)
+    with open('./data/raw_link.pkl', 'rb') as f:
+        link_list = pickle.load(f)
 
-with open('./data/raw_title.pkl', 'rb') as f:
-    title_list = pickle.load(f)
-with open('./data/raw_abst.pkl', 'rb') as f:
-    abst_list = pickle.load(f)
-with open('./data/raw_link.pkl', 'rb') as f:
-    link_list = pickle.load(f)
+    query_input = tokenizer(query_title, return_tensors='pt').to('cpu')
+    query1 = model(**query_input).pooler_output[0]
+    query_input = tokenizer(query_abst, return_tensors='pt').to('cpu')
+    query2 = model(**query_input).pooler_output[0]
 
-query_input = tokenizer(query_title, return_tensors='pt').to("cpu")
-query1 = model(**query_input).pooler_output[0]
-query_input = tokenizer(query_abst, return_tensors='pt').to("cpu")
-query2 = model(**query_input).pooler_output[0]
+    title_embeddings = np.load('./data/title_embeddings.npy')
+    abst_embeddings = np.load('./data/abst_embeddings.npy')
 
-title_embeddings = np.load('./data/title_embeddings.npy')
-abst_embeddings = np.load('./data/abst_embeddings.npy')
+    if use_title and use_abst:
+        query = torch.cat((query1, query2)).cpu().detach().numpy()
+        embeddings = np.concatenate([title_embeddings, abst_embeddings], axis=1)
+    elif use_title:
+        query = query1.cpu().detach().numpy()
+        embeddings = title_embeddings
+    elif use_abst:
+        query = query2.cpu().detach().numpy()
+        embeddings = abst_embeddings
+    else:
+        raise ValueError
 
-if use_title and use_abst:
-    query = torch.cat((query1, query2)).cpu().detach().numpy()
-    embeddings = np.concatenate([title_embeddings, abst_embeddings], axis=1)
-elif use_title:
-    query = query1.cpu().detach().numpy()
-    embeddings = title_embeddings
-elif use_abst:
-    query = query2.cpu().detach().numpy()
-    embeddings = abst_embeddings
-else:
-    raise ValueError
-
-sim_list = []
-start_time = time.time()
-for vector in embeddings:
-    sim_list.append(cos_similarity(query, vector))
+    sim_list = []
+    start_time = time.time()
+    for vector in embeddings:
+        sim_list.append(cos_similarity(query, vector))
         
-sim_list = np.array(sim_list)
-sim_idx = np.argsort(sim_list)[::-1]
-for i in range(top_n):
-    print('Similarlity: {}'.format(sim_list[sim_idx[i]]))
-    print('Title: {}'.format(title_list[sim_idx[i]]))
-    print('Link: {}'.format(link_list[sim_idx[i]]))
-    print('Abstract: \n{}'.format(abst_list[sim_idx[i]]))
-
-print(time.time() - start_time)
-
-with open("./data/past-result.txt", mode='w') as f:
+    sim_list = np.array(sim_list)
+    sim_idx = np.argsort(sim_list)[::-1]
     for i in range(top_n):
-        f.write('Similarlity: {}\n'.format(sim_list[sim_idx[i]]))
-        f.write('Title: {}\n'.format(title_list[sim_idx[i]]))
-        f.write('Link: {}\n'.format(link_list[sim_idx[i]]))
-        f.write('Abstract: \n{}\n\n'.format(abst_list[sim_idx[i]]))
+        print('Similarlity: {}'.format(sim_list[sim_idx[i]]))
+        print('Title: {}'.format(title_list[sim_idx[i]]))
+        print('Link: {}'.format(link_list[sim_idx[i]]))
+        print('Abstract: \n{}'.format(abst_list[sim_idx[i]]))
+
+    print(time.time() - start_time)
+
+    with open('./data/results.txt', mode='w') as f:
+        for i in range(top_n):
+            f.write('Similarlity: {}\n'.format(sim_list[sim_idx[i]]))
+            f.write('Title: {}\n'.format(title_list[sim_idx[i]]))
+            f.write('Link: {}\n'.format(link_list[sim_idx[i]]))
+            f.write('Abstract: \n{}\n\n'.format(abst_list[sim_idx[i]]))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--top-n', type=int, default=10)
+    parser.add_argument('--title', type=str, default='')
+    parser.add_argument('--abst', type=str, default='')
+    args = parser.parse_args()
+    main(args)
